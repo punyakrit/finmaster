1f38d3b56b36dff1e843cd95143bd92d273a6cea
Aman-Raut <amanraut13@gmail.com>
2023-02-24 23:45:47 +0530

    connect screen

---
+++ lib/Screen/home/add.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class Add_Screen extends StatefulWidget {
+  const Add_Screen({super.key});
+
+  @override
+  State<Add_Screen> createState() => _Add_ScreenState();
+}
+
+class _Add_ScreenState extends State<Add_Screen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey.shade100,
+      body: SafeArea(
+        child: Stack(
+          alignment: AlignmentDirectional.center,
+          children: [
+            background_container(context),
+            Positioned(
+              top: 120,
+              child: main_container(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/Screen/home/statistics.dart
+++ lib/Screen/home/statistics.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:finmaster/Screen/home/Chart.dart';
+import 'package:finmaster/Screen/home/top.dart';
+import 'package:flutter/material.dart';
 
 class Statistics extends StatefulWidget {
   const Statistics({super.key});
@@ -70,9 +73,105 @@ class _StatisticsState extends State<Statistics> {
                     ],
                   ),
                 ),
+                SizedBox(height: 20),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 15),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Container(
+                        width: 120,
+                        height: 40,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            Text(
+                              'Expense',
+                              style: TextStyle(
+                                  color: Colors.grey,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            Icon(
+                              Icons.arrow_downward_sharp,
+                              color: Colors.grey,
+                            )
+                          ],
+                        ),
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.grey,
+                            width: 2,
+                          ),
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Chart(),
+                SizedBox(
+                  height: 20,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 15),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Top Spending',
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      Icon(
+                        Icons.swap_vert,
+                        size: 25,
+                        color: Colors.grey,
+                      ),
+                    ],
+                  ),
+                )
               ],
             ),
-          )
+          ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (context, index) {
+                return ListTile(
+                  leading: Image.asset(
+                    'lib/Screen/home/${geter_top()[index].image!}',
+                    height: 40,
+                  ),
+                  title: Text(
+                    geter_top()[index].name!,
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  subtitle: Text(
+                    geter_top()[index].time!,
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  trailing: Text(
+                    geter_top()[index].fee!,
+                    style: TextStyle(
+                      fontSize: 19,
+                      color: Colors.red,
+                      fontWeight: FontWeight.bold,),),
+                );
+              },
+              childCount: geter_top().length,
+            ),
+          ),
         ],
       ),
     ));
